dd8f498f2278b1145de7a561aa56ca1cdb359927
Aleksandra Vorobeva <V-Sash@mail.ru>
2021-06-28 15:02:39 +0300

    2

--- av_blog/wsgi.py
+++ av_blog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for composeexample project.
+WSGI config for av_blog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'av_blog.settings')
 
 application = get_wsgi_application()
