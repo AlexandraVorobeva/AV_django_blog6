21c82ff3698daeb6e44accb9d4009aad57e8d0ab
Aleksandra Vorobeva <V-Sash@mail.ru>
2021-07-02 15:00:26 +0300

    3

---
+++ app/main_part/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def projects(request):
+    return render(request, 'works.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
